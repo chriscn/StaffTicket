40b0adf935958789dd7c26c88054ca4b7a6f2871
Christopher Nethercott <ccnethercott@gmail.com>
2020-08-03 14:39:56 +0100

    Added four levels of permission

--- src/main/java/github/chriscn/staffticket/StaffTicket.java
+++ src/main/java/github/chriscn/staffticket/StaffTicket.java
@@ -1,12 +1,24 @@
 package github.chriscn.staffticket;
 
+import org.bukkit.Bukkit;
+import org.bukkit.permissions.Permission;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class StaffTicket extends JavaPlugin {
 
+    public Permission stOpen = new Permission("staffticket.open");
+    public Permission stHelper = new Permission("staffticket.helper");
+    public Permission stModerator = new Permission("staffticket.moderator");
+    public Permission stAdmin = new Permission("staffticket.admin");
+    
     @Override
     public void onEnable() {
         // Plugin startup logic
+
+        Bukkit.getPluginManager().addPermission(stOpen);
+        Bukkit.getPluginManager().addPermission(stHelper);
+        Bukkit.getPluginManager().addPermission(stModerator);
+        Bukkit.getPluginManager().addPermission(stAdmin);
 
     }
 

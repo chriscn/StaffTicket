57815ff8c42534a24f1c55e86e6811b496447916
Christopher Nethercott <ccnethercott@gmail.com>
2020-08-03 14:58:36 +0100

    Starting ticket command

---
+++ src/main/java/github/chriscn/staffticket/command/TicketCommand.java
@@ -0,0 +1,27 @@
+package github.chriscn.staffticket.command;
+
+import github.chriscn.staffticket.StaffTicket;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
+
+import java.util.List;
+
+public class TicketCommand implements TabExecutor {
+
+    StaffTicket plugin;
+    public TicketCommand(StaffTicket instance) {
+        this.plugin = instance;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        return false;
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return null;
+    }
+}

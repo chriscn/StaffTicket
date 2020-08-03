4cb4cc1ed95a0c7da9195790c8aff9aec6d38cb5
Christopher Nethercott <ccnethercott@gmail.com>
2020-08-03 14:36:38 +0100

    Init commit of java project

---
+++ src/main/java/github/chriscn/staffticket/StaffTicket.java
@@ -0,0 +1,17 @@
+package github.chriscn.staffticket;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class StaffTicket extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}

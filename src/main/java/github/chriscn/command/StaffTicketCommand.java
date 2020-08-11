0349992d633be0f726e11eddfaacea55f142819d
Christopher Nethercott <ccnethercott@gmail.com>
2020-08-11 13:28:37 +0100

    Added basic option processing

--- src/main/java/github/chriscn/command/StaffTicketCommand.java
+++ src/main/java/github/chriscn/command/StaffTicketCommand.java
@@ -1,9 +1,21 @@
 package github.chriscn.command;
 
 import github.chriscn.StaffTicket;
+import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
 
 public class StaffTicketCommand implements CommandExecutor {
-    public StaffTicketCommand(StaffTicket staffTicket) {
+
+    StaffTicket plugin;
+    public StaffTicketCommand(StaffTicket instance) {
+        this.plugin = instance;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        String firstOption = args[0].toLowerCase();
+        
+        return false;
     }
 }

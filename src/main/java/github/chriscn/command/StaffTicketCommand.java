8f67871e95b768b36fdf93c908914593f1400444
Christopher Nethercott <ccnethercott@gmail.com>
2020-08-11 13:51:52 +0100

    Added reload infrastructure

--- src/main/java/github/chriscn/command/StaffTicketCommand.java
+++ src/main/java/github/chriscn/command/StaffTicketCommand.java
@@ -1,12 +1,14 @@
 package github.chriscn.command;
 
 import github.chriscn.StaffTicket;
+import github.chriscn.api.SQLManager;
 import net.md_5.bungee.api.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabExecutor;
 
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -26,6 +28,26 @@ public class StaffTicketCommand implements TabExecutor {
         String firstOption = args[0].toLowerCase();
 
         if (Arrays.asList(args).contains(firstOption)) {
+            if (firstOption.equalsIgnoreCase("reload")) {
+                sender.sendMessage(ChatColor.GREEN + "Attempting to reload the config.");
+
+                plugin.reloadConfig();
+                if (plugin.SUCCESSFUL_CONNECTION) {
+                    try {
+                        plugin.sql.getConnection().close();
+                        sender.sendMessage(ChatColor.GREEN + "Successfully closed the old SQL connection.");
+                    } catch (SQLException e) {
+                        sender.sendMessage(ChatColor.RED + "Failed to close the SQL Connection. Disabling this plugin.");
+                        plugin.getPluginLoader().disablePlugin(plugin);
+                    }
+                }
+
+                plugin.sql = null; // clears the old sql manager
+                plugin.sql = new SQLManager(plugin); // reinitalises the sql manager
+
+                sender.sendMessage(ChatColor.GREEN + "Successfully reloaded!");
+            }
+
             return true;
         } else {
             sender.sendMessage(ChatColor.RED + "Unknown Option.");

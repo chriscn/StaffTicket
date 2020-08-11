6e0d1de4fb279cd6f25a3a54935a77ccfcf46f95
Christopher Nethercott <ccnethercott@gmail.com>
2020-08-11 13:35:19 +0100

    Added onTabComplete

--- src/main/java/github/chriscn/command/StaffTicketCommand.java
+++ src/main/java/github/chriscn/command/StaffTicketCommand.java
@@ -1,11 +1,20 @@
 package github.chriscn.command;
 
 import github.chriscn.StaffTicket;
+import net.md_5.bungee.api.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.command.TabExecutor;
 
-public class StaffTicketCommand implements CommandExecutor {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class StaffTicketCommand implements TabExecutor {
+
+    String[] options = {"reload"};
+    List<String> commandOptions = Arrays.asList(options);
 
     StaffTicket plugin;
     public StaffTicketCommand(StaffTicket instance) {
@@ -15,7 +24,17 @@ public class StaffTicketCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         String firstOption = args[0].toLowerCase();
-        
-        return false;
+
+        if (Arrays.asList(args).contains(firstOption)) {
+            return true;
+        } else {
+            sender.sendMessage(ChatColor.RED + "Unknown Option.");
+            return false;
+        }
+    }
+
+    @Override
+    public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        return commandOptions;
     }
 }

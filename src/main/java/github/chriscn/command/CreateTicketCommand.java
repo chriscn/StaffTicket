d87083825e94d6fa9b90001319737d699edf9dcc
Christopher Nethercott <ccnethercott@gmail.com>
2020-08-16 17:39:36 +0100

    Create CreateTicketCommand

---
+++ src/main/java/github/chriscn/command/CreateTicketCommand.java
@@ -0,0 +1,9 @@
+package github.chriscn.command;
+
+import github.chriscn.StaffTicket;
+import org.bukkit.command.CommandExecutor;
+
+public class CreateTicketCommand implements CommandExecutor {
+    public CreateTicketCommand(StaffTicket staffTicket) {
+    }
+}

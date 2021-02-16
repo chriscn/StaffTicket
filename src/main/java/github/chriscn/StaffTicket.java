9be8ea17b2d8abc86d777ef4f476ff8433fd3a3d
Christopher Nethercott <ccnethercott@gmail.com>
2021-02-16 10:31:44 +0000

    Added basic onEnable() etc.

--- src/main/java/github/chriscn/StaffTicket.java
+++ src/main/java/github/chriscn/StaffTicket.java
@@ -1,2 +1,19 @@
-package github.chriscn;public class StaffTicket {
+package github.chriscn;
+
+import org.bukkit.plugin.PluginDescriptionFile;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class StaffTicket extends JavaPlugin {
+
+    PluginDescriptionFile pdfFile = this.getDescription();
+
+    @Override
+    public void onEnable() {
+        getLogger().info("Enabling " + pdfFile.getName() + " v" + pdfFile.getVersion());
+    }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Disabling " + pdfFile.getName() + " v" + pdfFile.getVersion());
+    }
 }
